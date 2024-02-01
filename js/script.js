1540d4d556ca2b01c468c60217e998efc267e8fa
AvavSam <avav2608005@gmail.com>
2024-02-01 23:45:11 +0800

    final

--- js/script.js
+++ js/script.js
@@ -7,19 +7,20 @@ const button = document.getElementById('tombol-kirim')
 button.addEventListener('click', function() {
     let nama = inputName.value;
     let email = inputEmail.value;
+    let interest = inputInterest.value;
 
     if (!nama) {
         const nameError = document.getElementById('name-error');
         nameError.classList.add('error-message');
 
-        nameError.innerText = "nama harus diisi";
+        nameError.innerText = "Nama harus diisi";
     }
     
     if (!email) {
         const emailError = document.getElementById('email-error');
         emailError.classList.add('error-message');
 
-        emailError.innerText = "email harus diisi";
+        emailError.innerText = "Email harus diisi";
     }
 
     if (!interest) {
